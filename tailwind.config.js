9937ea52a42a1132fa585e080afa894dbe01d7a3
zydnrbrn <zidanreborn@gmail.com>
2022-10-08 15:14:19 +0700

    change tailwindcss without cdn and update content

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}","./index.html","./article/**/*/{html},{js}"],
+
+darkMode: 'class',
+theme: {
+fontFamily: {
+    'righteous': ['Righteous'],
+    'kanit': ['Kanit']
+},
+extend: {
+  colors: {
+    'main-red': '#BB3853'
+  }
+}
+}
+}
